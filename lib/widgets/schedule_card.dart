ff41e39df49454d25d6e807b3ade33e1fc3c2dbd
Jason Park <jason808@naver.com>
2024-06-19 16:52:26 +0900

    약속 preview에서 Schedult로 가기

--- lib/widgets/schedule_card.dart
+++ lib/widgets/schedule_card.dart
@@ -1,28 +1,35 @@
 import "package:flutter/material.dart";
+import "package:promise_schedule/screens/schedule_detail_screen.dart";
 
 class SchedulePreviewCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(5))),
-      color: Colors.blue,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "Title",
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              "description",
-              style: TextStyle(fontSize: 15),
-            ),
-            Image.asset("assets/images/schedule_preview_sample.jpg")
-          ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => ScheduleScreen()));
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(5))),
+        color: Colors.blue,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Title",
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                "description",
+                style: TextStyle(fontSize: 15),
+              ),
+              Image.asset("assets/images/schedule_preview_sample.jpg")
+            ],
+          ),
         ),
       ),
     );
